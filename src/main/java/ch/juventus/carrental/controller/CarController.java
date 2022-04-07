d24b75d237ceddc33438a84ac06047b52726e1e1
MeTh07 <thouirmehdi@hotmail.com>
2022-04-07 21:20:01 +0200

    Initial commit

---
+++ src/main/java/ch/juventus/carrental/controller/CarController.java
@@ -0,0 +1,17 @@
+package ch.juventus.carrental.controller;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CarController {
+
+    @GetMapping("/api/v1/helloworld")
+    public ResponseEntity<String> helloWorld(){
+        return new ResponseEntity<>("Hello xyzWorld", HttpStatus.OK);
+    }
+
+}
